49ddbd12275769a9c8cc0436be2b87ee3a4ed777
Thiago <bliscosque@yahoo.com.br>
2023-11-23 10:44:49 -0600

    bit manip

--- intro_asm_book/c7_bits_ops/bits2.asm
+++ intro_asm_book/c7_bits_ops/bits2.asm
@@ -11,6 +11,19 @@ main:
     bts qword[set],8 ; 0b110010000
     bts qword[set+8],12 ;bit 76 (8*8+12) (COMO VER ESSE BIT NO GDB???)
 
+    mov rax, 76 ; bits 3,4,7 - 0b1001100
+    mov rbx, rax
+    shr rbx,6 ; 0b1
+    mov rcx,0x3f ; 6 bits da direita - 0x111111
+    xor edx,edx ; edx=0
+    lea rsi,[set]
+    bt [rsi+8*rbx],rcx ; test bit
+    bt [set+8*rbx],rcx ; another way... 
+    setc dl ;receive tested bit
+
+    bts [rsi+8*rbx],rcx ;set bit in "set" 10000000100000000011100
+    btr [rsi+8*rbx],rcx ;unset            10000000100000000011100
+
     xor rax,rax
     leave
     ret
